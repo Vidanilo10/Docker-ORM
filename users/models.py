51f848898c7a881ab32b95da20bf3f6868dff0ee
Victor Castañeda <vidanilo10@gmail.com>
2021-11-18 14:13:37 -0500

    Delete feed model

--- users/models.py
+++ users/models.py
@@ -46,15 +46,3 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     
     def __str__(self) -> str:
         return self.email
-
-class ProfileFeedItem(models.Model):
-    user_profile = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-
-    status_text: str = models.CharField(max_length=255)
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self) -> str:
-        return self.status_text
